d78da4599c4af9e0bcab9af3324f08426133541c
rifanazhari <rifan22si@mahasiswa.pcr.ac.id>
2023-12-11 11:04:57 +0700

    Update index

--- resources/views/Orangtua/index.blade.php
+++ resources/views/Orangtua/index.blade.php
@@ -16,7 +16,7 @@
             <div class="col-md-12">
                 <div class="card border-0 shadow rounded">
                     <div class="card-body">
-                        <a href="{{ route('Orangtua.create') }}" class="btn btn-md btn-success mb-3">TAMBAH Data Orangtua</a>
+                        <a href="{{ route('Orangtua.create') }}" class="btn btn-md btn-success mb-3">TAMBAH DATA ORANGTUA</a>
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
