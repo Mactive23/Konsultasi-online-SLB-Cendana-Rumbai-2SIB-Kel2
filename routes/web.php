119180f40513b10218feba45823d1d25cbde3185
Antony <antony22si@mahasiswa.pcr.ac.id>
2024-01-07 22:06:34 +0700

    data siswa dan jadwal konsultasi

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,24 @@ Route::resource('/siswa', \App\Http\Controllers\SiswaController::class);
 Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
 Route::get('/siswa/{id}', [SiswaController::class, 'show'])->name('siswa.show');
 
+// routes/web.php
+
+use App\Http\Controllers\JadwalKonsultasiController;
+
+Route::get('/jadwal_konsultasi', [JadwalKonsultasiController::class, 'index'])->name('jadwal_konsultasi.index');
+Route::get('/jadwal_konsultasi/create', [JadwalKonsultasiController::class, 'create'])->name('jadwal_konsultasi.create');
+Route::post('/jadwal_konsultasi', [JadwalKonsultasiController::class, 'store'])->name('jadwal_konsultasi.store'); // Perhatikan perubahan pada rute ini
+Route::get('/jadwal_konsultasi/edit/{id}', [JadwalKonsultasiController::class, 'edit'])->name('jadwal_konsultasi.edit');
+Route::put('/jadwal_konsultasi/update/{id}', [JadwalKonsultasiController::class, 'update'])->name('jadwal_konsultasi.update');
+Route::delete('/jadwal_konsultasi/destroy/{id}', [JadwalKonsultasiController::class, 'destroy'])->name('jadwal_konsultasi.destroy');
+
+Route::get('/layout', function () {
+    return view('layout.main');
+});
+
+Route::get('/layout', function () {
+    return view('layout.main1');
+});
+
+
+
